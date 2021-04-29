063613e72a2514d28167a745b91d6d2ab4d596b7
Klayver Ximenes <klayverx@gmail.com>
2021-04-29 11:53:37 -0300

    redirect

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,7 @@ export default function Login() {
 			Page login
 			<button>
 				<a
-					href="https://www.strava.com/oauth/authorize?client_id=65376&redirect_uri=http%3A%2F%2Flocalhost&response_type=code&scope=read_all,activity%3Aread_all,activity%3Awrite,profile%3Aread_all,profile%3Awrite"
+					href="https://www.strava.com/oauth/authorize?client_id=65376&redirect_uri=https%3A%2F%2Fstats-kx.vercel.app%2Fhome&response_type=code&scope=read_all,activity%3Aread_all,activity%3Awrite,profile%3Aread_all,profile%3Awrite"
 					target="_blank"
 					rel="noreferrer"
 				>
