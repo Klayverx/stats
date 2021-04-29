52d7fe9851e0c5173969ae333f8b86914580e265
Klayver Ximenes <klayverx@gmail.com>
2021-04-29 11:52:23 -0300

    button home

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,13 +7,16 @@ export default function Login() {
 	return (
 		<div>
 			Page login
-			<button
-				onClick={() => {
-					history.push('/home');
-				}}
-			>
-				Login com strava
+			<button>
+				<a
+					href="https://www.strava.com/oauth/authorize?client_id=65376&redirect_uri=http%3A%2F%2Flocalhost&response_type=code&scope=read_all,activity%3Aread_all,activity%3Awrite,profile%3Aread_all,profile%3Awrite"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Login com o strava
+				</a>
 			</button>
+			<button onClick={() => history.push('/home')}>Ir para home</button>
 		</div>
 	);
 }
