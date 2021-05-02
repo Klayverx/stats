b1a4ede461e6d2e59f541445c6212d99e3ea1f51
Klayver Ximenes <klayverx@gmail.com>
2021-05-01 23:22:13 -0300

    refatoração Home e Login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,16 @@
 import React, {useEffect, useState} from 'react';
 
-import {api, apiToken} from '../services/api';
+import {api} from '../services/api';
 
-function Home() {
-	const client_id = 65376;
-	const client_secret = '9141aecf4bc807456509b80235c856034984ce38';
-	const grant_type = 'authorization_code';
-
-	// const [firstName, setFirstName] = useState('');
+function Home(props) {
+	// const token = localStorage.getItem('access_token');
+	const [firstName, setFirstName] = useState('');
 	// const [lastName, setLastName] = useState('');
 	// const [imgProfile, setImageProfile] = useState('');
 	// const [city, setCity] = useState('');
 	// const [state, setState] = useState('');
-	const [accessToken, setAccessToken] = useState('');
-	// const [refreshToken, setRefreshToken] = useState('');
 
 	useEffect(() => {
-		let positionCode = window.location.href.indexOf('code=') + 5;
-		let endPositionCode = window.location.href.indexOf('&scope');
-
-		let code = window.location.href.slice(positionCode, endPositionCode);
-
 		// setFirstName(resp.data.athlete.firstname);
 		// setLastName(resp.data.athlete.lastname);
 		// setImageProfile(resp.data.athlete.profile);
@@ -28,27 +18,17 @@ function Home() {
 		// setState(resp.data.athlete.state);
 		// setRefreshToken(resp.data.refresh_token);
 
-		apiToken
-			.post(
-				`token?client_id=${client_id}&client_secret=${client_secret}&code=${code}&grant_type=${grant_type}`
-			)
-			.then(resp => {
-				console.log(resp);
-				console.log(resp.data);
-				console.log(resp.data.access_token);
-				api.get('athlete', {
-					headers: {
-						Authorization: `Bearer ${resp.data.access_token}`,
-					},
-				}).then(resp => {
-					console.log(resp);
-				});
-
-				setAccessToken(resp.data.access_token);
-			});
+		api.get('athlete', {
+			// headers: {
+			// 	Authorization: `Bearer ${token}`,
+			// },
+		}).then(resp => {
+			console.log(resp);
+			setFirstName(resp.firstname);
+		});
 	}, []);
 
-	return <div>Home page {accessToken}</div>;
+	return <div>Home page - {firstName} </div>;
 }
 
 export default Home;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import {useHistory} from 'react-router';
+// import Home from './Home';
 
 export default function Login() {
+	const redirect_uri = 'http://localhost:3000/auth';
+	const scope = 'read,profile:read_all,activity:read,activity:write';
+
 	const history = useHistory();
+	// const token = localStorage.getItem('access_token');
 
 	return (
-		<div>
-			<h1>Page login</h1>
-			<button>
-				<a
-					href="https://www.strava.com/oauth/authorize?client_id=65376&redirect_uri=http://localhost:3000/home&response_type=code&scope=read_all,activity%3Aread_all,activity%3Awrite,profile%3Aread_all,profile%3Awrite"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Login com o strava
-				</a>
-			</button>
-			<button onClick={() => history.push('/home')}>Ir para home</button>
-		</div>
+		<>
+			{/* {token ? (
+				<Home />
+			) : ( */}
+			<div>
+				<h1>Page login</h1>
+				<button>
+					<a
+						href={`https://www.strava.com/oauth/authorize?client_id=65376&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}`}
+						target="_blank"
+						rel="noreferrer"
+					>
+						Login com o strava
+					</a>
+				</button>
+				<button onClick={() => history.push('/home')}>
+					Ir para home
+				</button>
+			</div>
+			{/* )} */}
+		</>
 	);
 }
