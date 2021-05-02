68ea6c34a5c4f56e7c7e4c7b4d82d1e5cbb1be4a
Klayver Ximenes <klayverx@gmail.com>
2021-05-01 23:22:20 -0300

    auth.js

---
+++ src/utils/auth.js
@@ -0,0 +1,7 @@
+// import {useState} from 'react';
+
+// const [accessToken, setAccessToken] = useState('');
+// const [refreshToken, setRefreshToken] = useState('');
+// const [expiresIn, setExpiresIn] = useState(0);
+
+// export {accessToken, setAccessToken};
