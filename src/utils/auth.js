b7e7d0fc896781a7ce1efcaa5059daa544cd7f00
Klayver Ximenes <klayverx@gmail.com>
2021-05-04 21:26:55 -0300

    delete

--- src/utils/auth.js
+++
@@ -1,7 +0,0 @@
-// import {useState} from 'react';
-
-// const [accessToken, setAccessToken] = useState('');
-// const [refreshToken, setRefreshToken] = useState('');
-// const [expiresIn, setExpiresIn] = useState(0);
-
-// export {accessToken, setAccessToken};
