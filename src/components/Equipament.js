aa78f4227c9a52772444a2ce67640e23923434eb
Klayver Ximenes <klayverx@gmail.com>
2021-05-14 22:17:14 -0300

    page equipaments

---
+++ src/components/Equipament.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Text } from '@chakra-ui/layout'
+import { Flex } from '@chakra-ui/layout'
+
+export default function Equipament() {
+	return (
+		<Flex justify="center" align="center" height="100%" bgColor="#777">
+			<Text color="white" fontSize="2xl">
+				Página de equipamentos em construção 👷🏽‍♂️🚧
+			</Text>
+		</Flex>
+	)
+}
