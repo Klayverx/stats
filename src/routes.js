6e78943552f3228e024c9986e46ecc5375e34de8
Klayver Ximenes <klayverx@gmail.com>
2021-05-14 22:45:01 -0300

    layout routes

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import Login from './pages/Login';
-import Layout from './pages/Layout';
-import Auth from './pages/Auth';
-import NotFound from './pages/NotFound';
+import Equipament from './components/Equipament'
+import Create from './components/Create'
+import Stats from './components/Stats'
+import Login from './pages/Login'
+import Layout from './pages/Layout'
+import Auth from './pages/Auth'
+import NotFound from './pages/NotFound'
 
 function Routes() {
 	return (
 		<BrowserRouter>
 			<Switch>
 				<Route exact path="/" component={Login} />
-				<Route path="/home" component={Layout} />
+				<Layout>
+					<Route path="/stats" component={Stats} />
+					<Route path="/create" component={Create} />
+					<Route path="/equipaments" component={Equipament} />
+				</Layout>
 				<Route path="/auth" component={Auth} />
 				<Route component={NotFound} />
 			</Switch>
 		</BrowserRouter>
-	);
+	)
 }
 
-export default Routes;
+export default Routes
