2a3db9c9f489ea74b0ec191a2f0af61884097cb8
Klayver Ximenes <klayverx@gmail.com>
2021-05-14 22:17:04 -0300

    page create activity

---
+++ src/components/Create.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Text } from '@chakra-ui/layout'
+import { Flex } from '@chakra-ui/layout'
+
+export default function Create() {
+	return (
+		<Flex justify="center" align="center" height="100%" bgColor="#777">
+			<Text color="white" fontSize="2xl">
+				Página para criação de atividades em construção 👷🏽‍♂️🚧
+			</Text>
+		</Flex>
+	)
+}
